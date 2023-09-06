f5cd4be9570e5bbd8b84e9433d4598fac42a4410
Saransh-mittal <20ucs174@lnmiit.ac.in>
2023-09-06 15:13:41 +0530

    Made some user routes

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,56 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../model/userModel");
+const generateToken = require("../config/generateToken");
+const registerUser = asyncHandler(async (req,res) =>{
+    const {name, email, password, pic} = req.body;
+
+    if(!name || !email || !password)
+    {
+        res.status(400);
+        throw new Error("Please fill in all the details");
+    }
+
+    const userExists = await User.findOne({email});
+
+    if(userExists){
+        res.status(400);
+        throw new Error("Email already exits");
+    }
+
+    const user = await User.create({name,email,password,pic});
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email:user.email,
+            pic: user.pic,
+            token: generateToken(user._id)
+        });
+    }
+    else{
+        res.status(400);
+        throw new Error("failed to create new user");
+    }
+
+});
+
+const authUser = asyncHandler(async (req,res) =>{
+    const {email, password} = req.body;
+
+    if(!email || !password)
+    {
+        res.status(400);
+        throw new Error("Please fill in all the details");
+    }
+
+    const user = await User.findOne({email});
+    if(!user){
+        res.status(400);
+        throw new Error("Wrong credentials");
+    }
+
+    
+});
+
+module.exports = {registerUser};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const connectDB = require("./config/db");
 const app = express();
 dotenv.config();
+app.use(express.json());
+const userRoutes = require("./routes/userRoutes");
+
+connectDB();
 app.get('/',(req,res)=>{
     res.send(`App running on 5000`);
 });
+
+app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT;
 app.listen(5000,console.log(`Server started on port ${PORT}`));
