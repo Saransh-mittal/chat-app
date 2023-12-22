b3947ee3381acd9cd6bb5cd990f5cd3c5e13d527
Saransh Mittal <20ucs174@lnmiit.ac.in>
2023-12-22 17:56:05 +0530

    Added search user and chat APIs

--- backend/server.js
+++ backend/server.js
@@ -5,16 +5,18 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 connectDB();
-app.get('/',(req,res)=>{
-    res.send(`App running on 5000`);
+app.get("/", (req, res) => {
+  res.send(`App running on 5000`);
 });
 
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
-app.listen(5000,console.log(`Server started on port ${PORT}`));
+app.listen(5000, console.log(`Server started on port ${PORT}`));
